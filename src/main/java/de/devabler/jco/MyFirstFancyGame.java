53a9df693fba7a4130ce91e5fe5536ef90690243
Alexander Lehmann <a.lehm@gmx.de>
2020-04-27 18:25:28 +0200

    Liftoff!

--- src/main/java/de/devabler/jco/MyFirstFancyGame.java
+++ src/main/java/de/devabler/jco/MyFirstFancyGame.java
@@ -1,6 +1,9 @@
 package de.devabler.jco;
 
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 
 /**
@@ -9,8 +12,35 @@ import com.googlecode.lanterna.screen.Screen;
 public class MyFirstFancyGame {
 
     void runGame(Screen screen, TextGraphics textGraphics) throws Exception {
-        textGraphics.putString(0,0, "Hallo Welt!");
-        screen.refresh();
-        Thread.sleep(2000);
+        TerminalSize terminalSize = screen.getTerminalSize();
+
+        int playerCol = terminalSize.getColumns() / 2 - 3;
+        int playerRow = terminalSize.getRows() - 1;
+
+        while (true) {
+
+            textGraphics.putString(playerCol, playerRow, " -/\\- ");
+
+            KeyStroke input = screen.pollInput();
+            if (input != null) {
+                switch (input.getKeyType()) {
+                    case ArrowLeft:
+                        playerCol = playerCol - 1;
+                        break;
+                    case ArrowRight:
+                        playerCol = playerCol + 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if(input != null && input.getKeyType() == KeyType.Escape) {
+                break;
+            }
+
+            while(screen.pollInput() != null);
+            screen.refresh();
+            Thread.sleep(1000 / 10);
+        }
     }
 }
